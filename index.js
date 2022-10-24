842dc171361144897435deb7cb24ed3fbac278c4
Shapna <shapna088@gmail.com>
2022-10-24 16:52:17 +0600

    'added'

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const hotels = require('./data/data.json');
+
+app.get('/', (req, res) => {
+    res.send('hotel booking server is running')
+});
+
+app.get('/hotels', (req, res) => {
+    res.send(hotels)
+});
+
+app.get('/hotels/:id', (req, res) => {
+    const id = req.params.id;
+    const hotel = hotels.find(ht => ht.id === id);
+    res.send(hotel)
+});
+
+app.listen(port, () => {
+    console.log(`Hotel booking running on port ${port}`);
+});
